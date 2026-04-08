93642a190448c3610eec0e60d755d7bb811aa35b
grml <grml@grml.grml>
2026-04-08 22:46:32 +0800

    chore: removed terms.txt

--- benchmarks/filestream.ts
+++ benchmarks/filestream.ts
@@ -5,7 +5,7 @@ console.log("Seshat File-Streaming Benchmark");
 console.log("==============================");
 
 const trie = new Seshat();
-const filePath = "./textfiles/terms.txt";
+const filePath = "./textfiles/words.txt";
 const bufferSize = 16 * 1024 * 1024; // 16MB buffer
 
 // Start timing
